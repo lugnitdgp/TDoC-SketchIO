aa6b1e3062cc3868eeaad91677f79eade9da90b1
anjali252001 <73474345+anjali252001@users.noreply.github.com>
2021-10-28 09:16:55 +0530

    Task2

--- Anjali/backend/app.js
+++ Anjali/backend/app.js
@@ -3,14 +3,15 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
-const eventHandlers =require("./eventHandlers")(io);
+
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
 
+const io = socketio(server);
+const eventHandlers =require("./eventHandlers")(io);
 app.use(cors());
 app.use(router);
 
