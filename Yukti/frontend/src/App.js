625a3ec5dd58bfbe7b5630a6ed251ff3baf74aa8
Yukti <yuktikumari8719@gmail.com>
2021-10-28 22:05:16 +0530

    updated app.js

--- Yukti/frontend/src/App.js
+++ Yukti/frontend/src/App.js
@@ -6,18 +6,24 @@ import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
 import DrawArea from "./components/DrawArea";
 import LeaderBoard from "./components/LeaderBoard";
-/*
+
 const App = () => (
   <Router>
-    <Route path="/" exact component={Join} />
-    <Route path="/chat" exact component={Chat} />
+    <Route path="/" exact component={LeaderBoard} />
   </Router>
 );
-*/
 
+/*
 function App() {
   return <LeaderBoard />;
   //return <DrawArea />;
 }
+
+/*  <Route path="/chat" exact component={Chat} /> 
+ <Route path="/" exact component={Join} />
+ <Route path="/drawarea" exact component={DrawArea} />
+
+    <Route path="/leaderboard" exact component={LeaderBoard} />
+*/
 
 export default App;
